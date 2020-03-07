6734f5adb9e65e25af0f2f1583da42c01a30439f
wanchenghuang <1782331304@qq.com>
2020-03-07 21:20:14 +0800

    add files(数据源配置，切换成功事务控制失败--漏提交)

---
+++ chauncy-cloud-parent/chauncy-core/chauncy-config/src/main/java/com/chauncy/cloud/core/config/base/datasource/transaction/TransactionConfig.java
@@ -0,0 +1,44 @@
+package com.chauncy.cloud.core.config.base.datasource.transaction;
+
+import com.chauncy.cloud.common.constant.Constants;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author cheng
+ * @create 2020-03-07 19:21
+ *
+ * 第二数据源事务控制处理
+ *
+ * 多事物管理器配置
+ * 定义事务管理器 并指定其对应管理的数据源和声明name
+ *
+ * 不配置事物管理器，则默认数据源
+ */
+@Configuration
+public class TransactionConfig {
+
+    public final static String DEFAULT_TX = "defaultTx";
+
+    public final static String SECOND_TX = "slave1TX";
+
+
+    @Bean(name = TransactionConfig.DEFAULT_TX)
+    @Primary
+    public PlatformTransactionManager transaction(@Qualifier(Constants.MASTER) DataSource firstDataSource) {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager(firstDataSource);
+        return dataSourceTransactionManager;
+    }
+
+    @Bean(name = TransactionConfig.SECOND_TX)
+    public DataSourceTransactionManager rongyuanTransaction(@Qualifier(Constants.SLAVE1) DataSource rongyuanDataScoure) {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager(rongyuanDataScoure);
+        return dataSourceTransactionManager;
+    }
+}
