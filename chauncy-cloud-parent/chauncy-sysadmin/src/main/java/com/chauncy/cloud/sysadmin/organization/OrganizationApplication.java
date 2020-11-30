d70578df845f2ebc2ba1f7d7e9e4a9075fa5a586
cheng <1782331304@qq.com>
2020-11-30 12:51:40 +0800

    [add feature]xxl-job-handler

--- chauncy-cloud-parent/chauncy-sysadmin/src/main/java/com/chauncy/cloud/sysadmin/organization/OrganizationApplication.java
+++ chauncy-cloud-parent/chauncy-sysadmin/src/main/java/com/chauncy/cloud/sysadmin/organization/OrganizationApplication.java
@@ -3,6 +3,7 @@ package com.chauncy.cloud.sysadmin.organization;
 import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import com.alicp.jetcache.anno.config.EnableMethodCache;
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+import job.core.EnableXxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -25,6 +26,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableCreateCacheAnnotation
 @EnableMethodCache(basePackages = "com.chauncy.cloud")
+@EnableXxlJob
 public class OrganizationApplication {
 
     public static void main(String[] args) {
