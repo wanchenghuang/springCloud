75c2db2b427cf967f3cc3a8f08b5b9f93d5114b6
cheng <1782331304@qq.com>
2020-12-06 16:53:50 +0800

    [update]使用上传到nexus的chauncy-base基础项目-注解方式使用xxl任务调度模块

--- chauncy-cloud-parent/chauncy-sysadmin/src/main/java/com/chauncy/cloud/sysadmin/organization/OrganizationApplication.java
+++ chauncy-cloud-parent/chauncy-sysadmin/src/main/java/com/chauncy/cloud/sysadmin/organization/OrganizationApplication.java
@@ -3,7 +3,7 @@ package com.chauncy.cloud.sysadmin.organization;
 import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import com.alicp.jetcache.anno.config.EnableMethodCache;
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
-import com.chauncy.cloud.job.core.EnableXxlJob;
+import com.chauncy.base.job.core.EnableXxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class} )
 @MapperScan("com.chauncy.cloud.*.mapper.*")
-@ComponentScan(basePackages = {"com.chauncy.cloud"})
+@ComponentScan(basePackages = {"com.chauncy.cloud","com.chauncy.base"})
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
 @Slf4j
